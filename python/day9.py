3adb2159b2e129448fb3b349113f7eab60229b9e
Matt Bargenquast <matt.bargenquast@gmail.com>
2019-01-09 17:46:37 +1000

    Day 9 Part 2 solution added

--- python/day9.py
+++ python/day9.py
@@ -40,5 +40,47 @@ while i < len(data):
         booty += data[i]
         i += 1
 
-print(len(booty))
+print(f"Part one: {len(booty)}")
 
+def decompress(s):
+
+    retlen = 0
+    i = 0
+    while i < len(s):
+        if s[i] == '(':
+            # find closing bracket
+            ei = s.find(')', i)
+
+            # extract marker info
+            m = re.match(marker_pattern, s[i:ei])
+            if m:
+                numchar = int(m.group(1))
+                repeat = int(m.group(2))
+            else:
+                print(f"borked: {s[i:ei]}")
+
+            # move to the end of the marker
+            i = ei + 1
+
+            # and build up our decompressed string
+            bits = s[i:i + numchar]
+
+            bl = decompress(bits)
+            retlen += bl
+            for j in range(1,repeat):
+                retlen += bl
+
+            # move to the end of the marker block
+            i += numchar
+
+        else:
+            # just chuck it on the end
+            retlen += 1
+            i += 1
+
+    return retlen
+
+bootylen = 0
+i = 0
+l = decompress(data)
+print(f"PArt two: {l}")
